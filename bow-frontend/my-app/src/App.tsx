fccb8d0796e7181def118584f7679f1d129a0660
Conor Fitzpatrick <conor2fitzpatrick@gmail.com>
2023-11-04 15:56:21 -0400

    Set up ye styled-components ahoy!

--- bow-frontend/my-app/src/App.tsx
+++ bow-frontend/my-app/src/App.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+
+
+
+
+
 
 function App() {
   return (
@@ -8,7 +15,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          YARRR WE SETUP    
         </p>
         <a
           className="App-link"
