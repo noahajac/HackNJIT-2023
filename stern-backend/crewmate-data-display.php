1af7efa1fa1ef5548056cfab463b8a75670b0103
AllanRoz <114365300+AllanRoz@users.noreply.github.com>
2023-11-04 14:27:58 -0400

    Backend

--- stern-backend/crewmate-data-display.php
+++ stern-backend/crewmate-data-display.php
@@ -5,7 +5,7 @@ $password = 'jkL`-"p])t}./Dk+';
 $database = "oceanman";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
+$conn = mysqli_connect($servername, $username, $password, $database);
 
 // Check connection
 if (!$conn) {
