a898b451135fe89f8accf66a9a6666655b89a8f0
Noah Jacobson <noah@noahjacobson.com>
2023-11-05 10:58:56 -0500

    fix(stern-backend): fix add vital DB query

--- stern-backend/views/add_vital.php
+++ stern-backend/views/add_vital.php
@@ -5,8 +5,8 @@ require_once('database.php');
 $body = json_decode(file_get_contents("php://input"));
 
 $sql = $db->prepare('INSERT INTO VitalMeasurement VALUES (:height, :weight, :bloodPressure, :addingPhysician, NOW(), null, :id);');
-$sql->bindValue(':height', $body -> Height);
-$sql->bindValue(':weight', $body -> Weight);
+$sql->bindValue(':height', $body -> height);
+$sql->bindValue(':weight', $body -> weight);
 $sql->bindValue(':bloodPressure', $body -> bloodPressure);
 $sql->bindValue(':addingPhysician', $body -> addingPhysician);
 $sql->bindValue(':id', $id);
