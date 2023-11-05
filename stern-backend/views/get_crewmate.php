15353a24b5e034ed333ee5c227dc5aaea2f833f3
Noah Jacobson <noah@noahjacobson.com>
2023-11-04 21:47:59 -0400

    fix(stern-backend): get_crewmate

--- stern-backend/views/get_crewmate.php
+++ stern-backend/views/get_crewmate.php
@@ -2,11 +2,10 @@
 
 require_once('database.php');
 
-echo $id;
-
 $sql = $db->prepare('SELECT * FROM Crewmate WHERE CrewmateID=:id;');
 $sql->bindValue(':id', $id);
 $sql->execute();
 $rows = $sql->fetchAll(PDO::FETCH_OBJ);
+$sql->closeCursor();
 header('Content-Type: application/json');
-print json_encode($rows);
+echo(json_encode($rows));
