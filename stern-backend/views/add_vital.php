6d1e784d1224d287e0510bd1576f517a2c667401
Noah Jacobson <noah@noahjacobson.com>
2023-11-05 10:29:14 -0500

    fix(stern-backend): fix vital adding

--- stern-backend/views/add_vital.php
+++ stern-backend/views/add_vital.php
@@ -4,15 +4,13 @@ require_once('database.php');
 
 $body = json_decode(file_get_contents("php://input"));
 
-$sql = $db->prepare('INSERT INTO VitalMeasurement VALUES (Height = :height, `Weight` = :`weight`, BloodPressure = :bloodPressure, AddingPhysician = :addingPhysician, TimeOfMeasure = :timeOfMeasure, VitalID = :vitalID, CrewmateID = :id);');
+$sql = $db->prepare('INSERT INTO VitalMeasurement VALUES (Height = :height, Weight = :weight, BloodPressure = :bloodPressure, AddingPhysician = :addingPhysician, TimeOfMeasure = NOW(), null, CrewmateID = :id);');
 $sql->bindValue(':height', $body -> Height);
 $sql->bindValue(':weight', $body -> Weight);
 $sql->bindValue(':BloodPressure', $body -> bloodPressure);
 $sql->bindValue(':AddingPhysician', $body -> addingPhysician);
-$sql->bindValue(':TimeOfMeasure', $body -> timeOfMeasure);
 $sql->bindValue(':BloodPressure', $body -> bloodPressure);
 $sql->bindValue(':BloodPressure', $body -> bloodPressure);
-$sql->bindValue(':VitalID', $body -> vitalID);
 $sql->bindValue(':CrewmateID', $id);
 $sql->execute();
 $sql->closeCursor();
