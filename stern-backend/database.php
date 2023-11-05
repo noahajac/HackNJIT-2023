20cae2f4dd70dff966a051fafe94fe3223858878
Noah Jacobson <noah@noahjacobson.com>
2023-11-04 20:03:04 -0400

    chore(stern-backend): update backend to use cloud SQL

--- stern-backend/database.php
+++ stern-backend/database.php
@@ -1,13 +1,7 @@
 <?php
 $socket = "/cloudsql/hacknjit2023-strawhats:us-east4:oceanman";
-$host = "35.245.44.75";
 $username = "root";
 $password = 'jkL`-"p])t}./Dk+';
 $database = "oceanman";
 
-// Google Database connection
-//$conn = mysqli_connect(null, $username, $password, $database, null, $socket);
-
-// Local Connection
-$conn = mysqli_connect($host, $username, $password, $database);
-?>
+$conn = mysqli_connect(null, $username, $password, $database, null, $socket);
