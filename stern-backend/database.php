74d2fb556a6a810af19463e5a9187f240b5d7262
Noah Jacobson <noah@noahjacobson.com>
2023-11-04 22:29:21 -0400

    dev db connection

--- stern-backend/database.php
+++ stern-backend/database.php
@@ -1,5 +1,6 @@
 <?php
-$dsn = 'mysql:unix_socket=/cloudsql/hacknjit2023-strawhats:us-east4:oceanman;dbname=oceanman';
+//$dsn = 'mysql:unix_socket=/cloudsql/hacknjit2023-strawhats:us-east4:oceanman;dbname=oceanman';
+$dsn = 'mysql:host=35.245.44.75;dbname=oceanman';
 $username = "root";
 $password = 'jkL`-"p])t}./Dk+';
 $database = "oceanman";
