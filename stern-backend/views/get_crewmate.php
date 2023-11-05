9f185032533ab1a2f36511e91d22c972cc183849
Noah Jacobson <noah@noahjacobson.com>
2023-11-04 21:47:59 -0400

    test

--- stern-backend/views/get_crewmate.php
+++ stern-backend/views/get_crewmate.php
@@ -7,6 +7,6 @@ echo $id;
 $sql = $db->prepare('SELECT * FROM Crewmate WHERE CrewmateID=:id;');
 $sql->bindValue(':id', $id);
 $sql->execute();
-$rows = $sql->fetchAll();
+$rows = $sql->fetchAll(PDO::FETCH_OBJ);
 header('Content-Type: application/json');
 print json_encode($rows);
