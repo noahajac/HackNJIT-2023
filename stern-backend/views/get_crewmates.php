6695a087bf4656bb0419af1043190c4a771001a0
AllanRoz <114365300+AllanRoz@users.noreply.github.com>
2023-11-04 21:59:44 -0400

    Argh the crewmates ship

--- stern-backend/views/get_crewmates.php
+++ stern-backend/views/get_crewmates.php
@@ -1,11 +1,23 @@
 <?php
-function get_crewmates_handler()
-{
-    require 'database.php';
 
-    $sql = "SELECT * FROM `Crewmate`";
-    $result = mysqli_query($conn, $sql);
-    $rows = mysqli_fetch_all($result);
-    header('Content-Type: application/json');
-    print json_encode($rows);
-}
+require_once('database.php');
+
+$sql = $db->prepare('SELECT * FROM Crewmate;');
+$sql->execute();
+$rows = $sql->fetchAll(PDO::FETCH_OBJ);
+$sql->closeCursor();
+header('Content-Type: application/json');
+echo(json_encode($rows));
+
+
+
+// function get_crewmates_handler()
+// {
+//     require 'database.php';
+
+//     $sql = "SELECT * FROM `Crewmate`";
+//     $result = mysqli_query($conn, $sql);
+//     $rows = mysqli_fetch_all($result);
+//     header('Content-Type: application/json');
+//     print json_encode($rows);
+// }
