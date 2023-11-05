77538a47b38ef7ddaeb2cc4b4a7b27b30400fed6
Noah Jacobson <noah@noahjacobson.com>
2023-11-05 08:56:24 -0500

    update crewmate delete

--- stern-backend/views/delete_crewmate.php
+++ stern-backend/views/delete_crewmate.php
@@ -5,11 +5,4 @@ require_once('database.php');
 $sql = $db->prepare('DELETE FROM Crewmate WHERE CrewmateID=:id;');
 $sql->bindValue(':id', $id);
 $sql->execute();
-<<<<<<< HEAD
 $sql->closeCursor();
-=======
-$rows = $sql->fetchAll(PDO::FETCH_OBJ);
-$sql->closeCursor();
-header('Content-Type: application/json');
-echo(json_encode($rows));
->>>>>>> da172731453badab238d7c9ccfea8ec0824973fc
