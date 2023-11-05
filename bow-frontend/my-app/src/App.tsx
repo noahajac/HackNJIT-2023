476a97b6d60c3684640d53edd571b5402fc63073
Conor Fitzpatrick <conor2fitzpatrick@gmail.com>
2023-11-04 23:15:59 -0400

    Ahoy maties, committing this so we can avoid mixed sea charts in the future.

--- bow-frontend/my-app/src/App.tsx
+++ bow-frontend/my-app/src/App.tsx
@@ -1,13 +1,21 @@
   import React from 'react';
   import styled from 'styled-components';
-
+ 
 import Crewtable from './components/Crewtable';
+import { Route, Routes } from 'react-router-dom';
 
   function App() {
 
     return (
       <>
-      <Crewtable/>
+
+    <div className="App">
+      <Routes>
+        <Route path="/crew" element={ <Crewtable/> } />
+      </Routes>
+    </div>
+  
+
     </>
     );
   }
